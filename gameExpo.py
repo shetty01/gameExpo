75b1a10928043ee40a3a10bf15f5b06ad63b6fa2
Samod Shetty <samod.shetty@gmail.com>
2021-04-10 20:57:21 -0700

    Adding comments to code

--- gameExpo.py
+++ gameExpo.py
@@ -1,16 +1,22 @@
 print(f"\n\n****************************************************")
 print(f"*************    Welcome to Game Expo  *************")
 print(f"****************************************************\n")
+
+#Get Gender input from the user. 
 gender = input("Please enter your gender (B for Boy and G for Girl):")
 
+#Get Age as input from the user
 age = input("Please enter your age:")
 
+#Check if the age is integer. 
 try:
     age = int(age)
 except ValueError:
     print(f"Incorrect age. Please enter a number for age.")
     exit()
 
+#Ensure case doesn't cause any issue. so compare after converting the input string to Upper case and compare with B or BOY 
+#This is the predicate to compare the gender with Boy. 
 if( gender.upper() == 'B' or gender.upper() == 'BOY'):
     if(age < 6):
         print(f"\nCongratulations!! You have been enrolled to Rhyming\n\n")
